d6de425ab452d65c183e8902372fe37dc376e9da
Marvin Mante <marvinsjsu@gmail.com>
2014-08-12 00:23:37 -0700

    updated look

---
+++ js/main2.js
@@ -0,0 +1,37 @@
+$(function(){
+
+  $(".content").hide("fast", function(){
+    $("#myself").fadeIn(2000);
+  });
+
+
+  $(".controls, .control").on("click", function(event){
+    event.preventDefault();
+    var divName = $(this).attr("target");
+    hideAllShowOne(divName);
+  });
+
+  var hideAllShowOne = function(divName) {
+    $(".content").hide();
+    $("#" + divName).fadeIn("slow");
+  };
+
+
+  $(".details-container").hide(true);
+  $(".details-toggle").on("click", function(){
+    var button = this;
+    var target = $(this).attr("target");
+      $("#"+target+"-summary").slideToggle("slow");
+    $("#"+target).slideToggle("slow", function(){
+      var text = $(button).text();
+      var html = "";
+      if (text == "more details ...") {
+        html = "hide";
+      } else {
+        html = "more details ...";
+      }
+
+      $(button).html(html);
+    });
+  });
+});
